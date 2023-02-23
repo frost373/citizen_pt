797e3fb214902cafa73656099a42aa8a2f817033
dongbin3 <dongbin3@kingsoft.com>
2023-02-23 16:17:59 +0800

    init

--- smart_contract/coin/contracts/MyToken.sol
+++ smart_contract/coin/contracts/MyToken.sol
@@ -4,5 +4,10 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+    constructor() ERC20("DiaoMaoCoin", "DMC") {}
+
+
+     function safeMint(address to, uint256 amount) public  {
+        _mint(to, amount);
+    }
 }
